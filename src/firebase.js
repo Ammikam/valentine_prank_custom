edbd25af9159d610d68941ad3c82bcaa5bcb5baf
Ammikam <mutheeammikam5@gmail.com>
2026-02-10 12:29:40 +0300

    code refactor

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -16,5 +15,5 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const db = getDatabase(app);
